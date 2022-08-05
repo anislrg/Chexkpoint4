167c99ae6b7aedff698ee66ee297205dede6e0d9
anislrg <anislrg.perso@gmail.com>
2022-08-05 12:19:46 +0200

    calendar reservation okay

--- frontend/src/pages/AdminHome.jsx
+++ frontend/src/pages/AdminHome.jsx
@@ -1,8 +1,51 @@
 /* eslint-disable import/no-extraneous-dependencies */
+import axios from "@services/axios";
 import ClientList from "@components/ClientList";
 import TextEditor from "@components/TextEditor";
+import { Eventcalendar, toast, localeFr } from "@mobiscroll/react";
+import React, { useEffect, useState } from "react";
 
 export default function AdminHome() {
+  const [myEvents, setEvents] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [reservationTitle, setReservationTitle] = useState();
+
+  const postClient = async () => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}preorder`)
+      .then((res) => setReservationTitle(res.data));
+  };
+  const getDates = async () => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}preorder/dates`)
+      .then((res) => setEvents(res.data));
+  };
+
+  useEffect(() => {
+    getDates();
+    postClient();
+  }, []);
+  // React.useEffect(() => {
+  //   // getJson(
+  //   //   "https://trial.mobiscroll.com/events/?vers=5",
+  //   //   (events) => {
+  //   //     setEvents(events);
+  //   //   },
+  //   //   "jsonp"
+  //   // );
+
+  // }, []);
+  const onEventClick = React.useCallback((event) => {
+    toast({
+      message: event.data.title,
+    });
+  }, []);
+
+  const view = React.useMemo(() => {
+    return {
+      calendar: { popover: true, count: true },
+    };
+  }, []);
   return (
     <section className="background-home">
       <div className="position-admin--editor">
@@ -11,6 +54,19 @@ export default function AdminHome() {
         </div>
         <ClientList />
       </div>
+      <Eventcalendar
+        locale={localeFr}
+        theme="ios"
+        themeVariant="light"
+        clickToCreate={false}
+        dragToCreate={false}
+        dragToMove={false}
+        dragToResize={false}
+        eventDelete
+        data={myEvents}
+        view={view}
+        onEventClick={onEventClick}
+      />
     </section>
   );
 }
